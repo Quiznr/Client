b757bf2d2d3711223fde06df8f0f28ab1d291809
M4RT1N <mjlynch35@yahoo.com>
2023-07-31 11:04:39 -0500

    Cleaning frontend

--- src/Components/Navigation/Make-Quiz.js
+++ src/Components/Navigation/Make-Quiz.js
@@ -2,21 +2,21 @@ import React, { useEffect } from "react";
 import "../../css/quiz.css";
 
 export default function MakeQuiz() {
-  //   useEffect(() => {
-  //     fetch("/home")
-  //       .then((response) => {
-  //         if (!response.ok) {
-  //           throw new Error("Network response was not ok");
-  //         }
-  //         return response.json();
-  //       })
-  //       .then((data) => {
-  //         console.log(data);
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error fetching data:", error);
-  //       });
-  //   }, []);
+  useEffect(() => {
+    fetch("https://quizner-backend-836f3d753759.herokuapp.com/home")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
     <div className="makequiz-page">
