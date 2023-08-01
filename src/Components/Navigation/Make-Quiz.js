84c2c4b2f4f17f83a779f86844d1de99f4dd4345
M4RT1N <mjlynch35@yahoo.com>
2023-08-01 13:10:07 -0500

    Checking to see if errors persist

--- src/Components/Navigation/Make-Quiz.js
+++ src/Components/Navigation/Make-Quiz.js
@@ -4,24 +4,24 @@ import "../../css/quiz.css";
 export default function MakeQuiz() {
   const [backendData, setBackendData] = useState([]);
 
-  // const backendURL = process.env.REACT_APP_API_BASE_URL;
+  const backendURL = process.env.REACT_APP_API_BASE_URL;
 
-  // useEffect(() => {
-  //   fetch(`${backendURL}`)
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setBackendData(data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch(`${backendURL}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBackendData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   const [formData, setFormData] = useState({
     quiz_name: "",
@@ -76,60 +76,6 @@ export default function MakeQuiz() {
   return (
     <div className="makequiz-page">
       <h1 style={{ color: "white" }}>Make a Quiz</h1>
-      {/* <form className="makequiz-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="quiz_name"
-          value={formData.quiz_name}
-          placeholder="Quiz Name"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="quiz_description"
-          value={formData.quiz_description}
-          placeholder="Quiz Description"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="quiz_category"
-          value={formData.quiz_category}
-          placeholder="Quiz Category"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="quiz_difficulty"
-          value={formData.quiz_difficulty}
-          placeholder="Quiz Difficulty"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="quiz_score"
-          value={formData.quiz_score}
-          placeholder="Quiz Score"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="quiz_high_score"
-          value={formData.quiz_high_score}
-          placeholder="Quiz High Score"
-          onChange={handleChange}
-        />
-        <button type="submit">Submit Quiz</button>
-      </form> */}
-
-      <div className="quiz-list">
-        {/* {backendData.map((quiz) => (
-          <div className="quiz-card" key={quiz.id}>
-            <h2>{quiz.quiz_name}</h2>
-            <p>{quiz.quiz_description}</p>
-          </div>
-        ))} */}
-      </div>
     </div>
   );
 }
