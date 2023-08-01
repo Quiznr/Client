c608268943074d03d588b9e19311dbeb757ef567
M4RT1N <mjlynch35@yahoo.com>
2023-08-01 12:47:39 -0500

    Cleaning frontend

--- src/Components/Navigation/Make-Quiz.js
+++ src/Components/Navigation/Make-Quiz.js
@@ -32,46 +32,46 @@ export default function MakeQuiz() {
     quiz_high_score: "",
   });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
 
-    fetch(`${backendURL}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data); // Handle the response from the server, if needed
-        // Optionally, you can reset the form fields after successful submission:
-        setFormData({
-          quiz_name: "",
-          quiz_description: "",
-          quiz_category: "",
-          quiz_difficulty: "",
-          quiz_score: "",
-          quiz_high_score: "",
-        });
-      })
-      .catch((error) => {
-        console.error("Error submitting quiz:", error);
-      });
-  };
+  //   fetch(`${backendURL}`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(formData),
+  //   })
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data); // Handle the response from the server, if needed
+  //       // Optionally, you can reset the form fields after successful submission:
+  //       setFormData({
+  //         quiz_name: "",
+  //         quiz_description: "",
+  //         quiz_category: "",
+  //         quiz_difficulty: "",
+  //         quiz_score: "",
+  //         quiz_high_score: "",
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error submitting quiz:", error);
+  //     });
+  // };
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setFormData((prevFormData) => ({
+  //     ...prevFormData,
+  //     [name]: value,
+  //   }));
+  // };
 
   return (
     <div className="makequiz-page">
