28b10ae719a41e86871e54ba19ac6b9ae9c6794e
M4RT1N <mjlynch35@yahoo.com>
2023-08-01 12:58:37 -0500

    Checking to see if errors persist

--- src/Components/Navigation/Make-Quiz.js
+++ src/Components/Navigation/Make-Quiz.js
@@ -4,24 +4,24 @@ import "../../css/quiz.css";
 export default function MakeQuiz() {
   const [backendData, setBackendData] = useState([]);
 
-  // const backendURL = process.env.REACT_APP_API_BASE_URL;
+  const backendURL = process.env.REACT_APP_API_BASE_URL;
 
-  // useEffect(() => {
-  //   fetch(`${backendURL}`)
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setBackendData(data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch(`${backendURL}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBackendData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   const [formData, setFormData] = useState({
     quiz_name: "",
@@ -32,38 +32,38 @@ export default function MakeQuiz() {
     quiz_high_score: "",
   });
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-  //   fetch(`${backendURL}`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(formData),
-  //   })
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data); // Handle the response from the server, if needed
-  //       // Optionally, you can reset the form fields after successful submission:
-  //       setFormData({
-  //         quiz_name: "",
-  //         quiz_description: "",
-  //         quiz_category: "",
-  //         quiz_difficulty: "",
-  //         quiz_score: "",
-  //         quiz_high_score: "",
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error submitting quiz:", error);
-  //     });
-  // };
+    fetch(`${backendURL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data); // Handle the response from the server, if needed
+        // Optionally, you can reset the form fields after successful submission:
+        setFormData({
+          quiz_name: "",
+          quiz_description: "",
+          quiz_category: "",
+          quiz_difficulty: "",
+          quiz_score: "",
+          quiz_high_score: "",
+        });
+      })
+      .catch((error) => {
+        console.error("Error submitting quiz:", error);
+      });
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -76,7 +76,7 @@ export default function MakeQuiz() {
   return (
     <div className="makequiz-page">
       <h1 style={{ color: "white" }}>Make a Quiz</h1>
-      {/* <form className="makequiz-form" onSubmit={handleSubmit}>
+      <form className="makequiz-form" onSubmit={handleSubmit}>
         <input
           type="text"
           name="quiz_name"
@@ -120,7 +120,7 @@ export default function MakeQuiz() {
           onChange={handleChange}
         />
         <button type="submit">Submit Quiz</button>
-      </form> */}
+      </form>
 
       <div className="quiz-list">
         {/* {backendData.map((quiz) => (
